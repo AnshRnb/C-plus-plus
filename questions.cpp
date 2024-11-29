d034a90f23a959d3fa8d83cfa0dedc824cbd37c8
AnshRnb <anshrock@gmail.com>
2024-11-29 23:41:03 +0530

    uppercase & lower question added

--- questions.cpp
+++ questions.cpp
@@ -35,39 +35,101 @@
 
 // making  a pallindrome with string:::
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-void reverse(string str)
-{
-    string strrev = str;
-    int n = str.length();
-    for (int i = 0; i < n / 2; ++i)
-    {
-        int temp = 0;
-        temp = str[i];
-        str[i] = str[n - i - 1];
-        str[n - i - 1] = temp;
-    }
+// void reverse(string str)
+// {
+//     string strrev = str;
+//     int n = str.length();
+//     for (int i = 0; i < n / 2; ++i)
+//     {
+//         int temp = 0;
+//         temp = str[i];
+//         str[i] = str[n - i - 1];
+//         str[n - i - 1] = temp;
+//     }
 
-    for (int i = 0; i < n; ++i)
-    {
-        cout << str[i];
-    }
-    cout << endl;
+//     for (int i = 0; i < n; ++i)
+//     {
+//         cout << str[i];
+//     }
+//     cout << endl;
 
-    if (str == strrev)
-    {
-        cout << " pallindrome";
-    }else{
-         cout<<" not pallindrome";
-    }
-}
+//     if (str == strrev)
+//     {
+//         cout << " pallindrome";
+//     }else{
+//          cout<<" not pallindrome";
+//     }
+// }
 
-int main()
-{
-    string str;
-    cout << "enter your words  ";  //madam
-    cin >> str;
-    reverse(str);
-}
+// int main()
+// {
+//     string str;
+//     cout << "enter your words  ";  //madam
+//     cin >> str;
+//     reverse(str);
+// }
+
+// ----------------------------------------
+// making  a uppercase and lowercase with string:::
+
+// #include <iostream>
+// #include <cctype>
+// using namespace std;
+
+// void uppercase(string& str)
+// {
+//     for ( int i =0; i< str.length(); ++i)
+//     {
+//         str[i] =   toupper(str[i]);
+//     }
+// }
+
+
+// void lowercase(string& str)
+// {
+//     for ( int i =0; i< str.length(); ++i)
+//     {
+//         str[i] =   tolower(str[i]);
+//     }
+
+
+// }
+
+// int main()
+// {
+//     string str;
+//     cout<<"enter the word or anything  ";
+//     // cin>>str;
+//     getline(cin, str);
+
+//     int choice = 0;
+
+//     cout<<"make it uppercase press 1 "<<endl;
+//     cout<<"make it lowercase press 2 "<<endl;
+//     cout<<"choose it from anyone  1 and 2"<<endl;
+//     cin>>choice;
+
+//     if ( choice == 1)
+//     {
+//         uppercase(str);
+//         cout<<"uppercase : "<<str<<endl;
+        
+//     }else if  (choice == 2){
+//         lowercase(str);
+//         cout<<"lowercase : "<<str<<endl;
+
+//     }else{
+//         cout<<"invalid"<<endl;
+//     }
+
+// }
+
+
+//---------------------------------------------
+
+
+
+
