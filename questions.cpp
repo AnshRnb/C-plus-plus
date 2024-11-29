79a4fd67c511f62da8d43793364b2d7a29e8d609
AnshRnb <anshrock@gmail.com>
2024-11-29 19:55:28 +0530

    string pallindrome added

--- questions.cpp
+++ questions.cpp
@@ -1,33 +1,73 @@
-//making  a pallindrome:::
+// making  a pallindrome:::
+
+// #include <iostream>
+// using namespace std;
+
+// void pallindrome()
+// {
+//     int n;
+//     cin>>n;
+//     int rev = 0;
+//     int original = n;
+
+//     while( n > 0)
+//     {
+//         rev = rev * 10 + n % 10;
+//         n = n / 10;
+//     }
+
+//     cout<<rev;
+
+//     if ( original == rev){
+//         cout<<" pallindrome";
+//     }else{
+//         cout<<" not pallindrome";
+//     }
+// }
+
+// int main()
+// {
+//     pallindrome();
+//     return 0;
+// }
+
+// -----------------------------------------
+
+// making  a pallindrome with string:::
 
 #include <iostream>
 using namespace std;
 
-void pallindrome()
+void reverse(string str)
 {
-    int n;
-    cin>>n;
-    int rev = 0;
-    int original = n;
-
-    while( n > 0)
+    string strrev = str;
+    int n = str.length();
+    for (int i = 0; i < n / 2; ++i)
     {
-        rev = rev * 10 + n % 10;
-        n = n / 10;
+        int temp = 0;
+        temp = str[i];
+        str[i] = str[n - i - 1];
+        str[n - i - 1] = temp;
     }
 
-    cout<<rev;
+    for (int i = 0; i < n; ++i)
+    {
+        cout << str[i];
+    }
+    cout << endl;
 
-    if ( original == rev){
-        cout<<" pallindrome";
+    if (str == strrev)
+    {
+        cout << " pallindrome";
     }else{
-        cout<<" not pallindrome";
+         cout<<" not pallindrome";
     }
 }
 
 int main()
 {
-    pallindrome();
-    return 0;
+    string str;
+    cout << "enter your words  ";  //madam
+    cin >> str;
+    reverse(str);
 }
-
